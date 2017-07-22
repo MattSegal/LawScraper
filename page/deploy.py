3ffb1f8c9f94f4a134c030ca719b0f2cc2481baf
MattSegal <mattdsegal@gmail.com>
2017-07-22 18:03:37 +1000

    fix favicon upload

--- page/deploy.py
+++ page/deploy.py
@@ -10,16 +10,23 @@ import shutil
 
 bucket = boto3.resource('s3').Bucket('lawscraper.xyz')
 
+DO_NOT_COMPRESS = (
+    'png'
+)
+
+def get_file_extension(filename):
+    return filename.split('.')[-1]
+
 
 def get_content_type(filename):
-    extension = filename.split('.')[-1]
+    extension = get_file_extension(filename)
     content_types = {
         'html': 'text/html',
         'css': 'text/css',
         'jpeg': 'image/jpeg',
         'png': 'image/png',
         'gif': 'image/gif',
-        'ico': 'image/vnd.microsoft.icon',
+        'ico': 'image/x-icon',
         'js': 'application/javascript',
         'json': 'application/json'
     }
@@ -30,7 +37,9 @@ def upload(path='build/'):
     files = (
         os.path.join(path, node) for node in os.listdir(path)
         if os.path.isfile(os.path.join(path, node)) and
-        '.map' not in node # don't need 'em
+        '.map' not in node and # don't need 'em
+        'service-worker' not in node and # don't understand 'em
+        'asset-manifest' not in node # don't understand 'em
     )
     
     folders = (
@@ -42,19 +51,29 @@ def upload(path='build/'):
         upload(folder)
 
     for filename in files:
-        with open(filename, 'r') as f:
+        with open(filename, 'rb') as f:
             content_type = get_content_type(filename)
             print('uploading {} as {}... '.format(filename, content_type), end='')
-            f_compressed = BytesIO()
-            with gzip.GzipFile(fileobj=f_compressed, mode='wb') as gz:
-                shutil.copyfileobj(f, gz)
-            f_compressed.seek(0)
-            key = filename.replace('build/', '').replace('\\', '/')
-            bucket.upload_fileobj(f_compressed, key, {
+
+            file_extension = get_file_extension(filename)
+            should_compress = file_extension not in DO_NOT_COMPRESS
+            if should_compress:
+                f_compressed = BytesIO()
+                with gzip.GzipFile(fileobj=f_compressed, mode='wb') as gz:
+                    shutil.copyfileobj(f, gz)
+                f_compressed.seek(0)
+                f_upload = f_compressed
+            else:
+                f_upload = f
+
+            file_metadata = {
                 'ContentType': content_type, 
-                'ContentEncoding': 'gzip',
                 'ACL': 'public-read'
-            })
+            }
+            if should_compress:
+                file_metadata['ContentEncoding'] = 'gzip'
+            key = filename.replace('build/', '').replace('\\', '/')
+            bucket.upload_fileobj(f_upload, key, file_metadata)
             print('done')
 
 if __name__ == '__main__':
